952844ccd0b198b2d6f4ead7478715d93d9d680b
Rohit Shetty <rohitshetty@credenceanalytics.com>
2025-09-25 19:53:05 +0530

    fix: Changed default server port to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0'; // Listen on all interfaces
-const port = 8000;
+const port = 9000;
 
 const server = http.createServer((req, res) => {
       // New Health Check Logic
