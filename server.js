e1a43d78655cda625400ad294c3f7534a82703f9
Rohit Shetty <rohitshetty@credenceanalytics.com>
2025-09-25 19:38:30 +0530

    feat: Initial setup of Node.js basic server

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const http = require('http');
+
+const hostname = '0.0.0.0'; // Listen on all interfaces
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from the DevOps Microservice!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
